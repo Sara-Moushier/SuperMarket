4351f83c88eba6385dd5a9ce40294e2dd06aa9a9
Salma Zakaria <Salma2018170755@cis.asu.edu.eg>
2021-05-29 19:16:26 +0200

    SalmaChanges

---
+++ SuperMarket/Reciept.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace SuperMarket
+{
+    partial class Reciept
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TotalPrice = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(109, 140);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(515, 269);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(71, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(581, 109);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Your Reciept";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Nova Cond", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(109, 475);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 38);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Total Price";
+            // 
+            // TotalPrice
+            // 
+            this.TotalPrice.AutoSize = true;
+            this.TotalPrice.Font = new System.Drawing.Font("Arial Nova Cond", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TotalPrice.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.TotalPrice.Location = new System.Drawing.Point(357, 475);
+            this.TotalPrice.Name = "TotalPrice";
+            this.TotalPrice.Size = new System.Drawing.Size(92, 38);
+            this.TotalPrice.TabIndex = 3;
+            this.TotalPrice.Text = "label3";
+            this.TotalPrice.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Reciept
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(747, 547);
+            this.Controls.Add(this.TotalPrice);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Reciept";
+            this.Text = "Reciept";
+            this.Load += new System.EventHandler(this.Reciept_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label TotalPrice;
+    }
+}
---
+++ SuperMarket/Reciept.cs
@@ -0,0 +1,54 @@
+﻿using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuperMarket
+{
+    public partial class Reciept : Form
+    {
+        OracleDataAdapter adapter;
+        OracleCommandBuilder builder;
+        DataSet DS;
+        string constr = "data source=orcl; user id=scott; password=tiger;";
+        string cmdstr = "";
+        public Reciept()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void Reciept_Load(object sender, EventArgs e)
+        {
+            cmdstr = "select pname, quantity from ordereditems where OID =:id";
+            adapter = new OracleDataAdapter(cmdstr, constr);
+            adapter.SelectCommand.Parameters.Add("id", Order.OID);
+            DS = new DataSet();
+            adapter.Fill(DS);
+            dataGridView1.DataSource = DS.Tables[0];
+
+            cmdstr = "select price from customerorder where OID =:id";
+            adapter = new OracleDataAdapter(cmdstr, constr);
+            adapter.SelectCommand.Parameters.Add("id", Order.OID);
+            DS = new DataSet();
+            adapter.Fill(DS);
+            TotalPrice.Text = DS.Tables[0].Rows[0]["Price"].ToString();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
