b5a281b7b588fbaff5e477276bced7e403549d2c
Sara <saranor@gmail.com>
2021-05-30 16:54:35 +0200

    Revert "Merge pull request #3 from Sara-Moushier/SalmaBranch"
    This reverts commit 95da229dedb8b27fb3dedad9120bb3b0aaf9e828, reversing
    changes made to 59998f6b9ed4f7df1c1a442869c3d4584a660cee.

--- SuperMarket/Order.Designer.cs
+++ SuperMarket/Order.Designer.cs
@@ -29,12 +29,18 @@ namespace SuperMarket
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Order));
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Item = new System.Windows.Forms.TextBox();
+            this.Price = new System.Windows.Forms.TextBox();
             this.AddOrder = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.Products = new System.Windows.Forms.Button();
-            this.ViewOrder = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.Products = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Quant = new System.Windows.Forms.TextBox();
+            this.AddItem = new SuperMarket.CircleButton();
             this.SuspendLayout();
             // 
             // label1
@@ -47,9 +53,50 @@ namespace SuperMarket
             this.label1.TabIndex = 0;
             this.label1.Text = "Orders";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Nova Cond", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(83, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 38);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Item";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Nova", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(83, 369);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 38);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Price";
+            // 
+            // Item
+            // 
+            this.Item.BackColor = System.Drawing.Color.White;
+            this.Item.Enabled = false;
+            this.Item.Font = new System.Drawing.Font("Arial Nova", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Item.Location = new System.Drawing.Point(235, 206);
+            this.Item.Name = "Item";
+            this.Item.Size = new System.Drawing.Size(176, 46);
+            this.Item.TabIndex = 3;
+            // 
+            // Price
+            // 
+            this.Price.BackColor = System.Drawing.Color.White;
+            this.Price.Enabled = false;
+            this.Price.Font = new System.Drawing.Font("Arial Nova Cond", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Price.Location = new System.Drawing.Point(235, 369);
+            this.Price.Name = "Price";
+            this.Price.Size = new System.Drawing.Size(176, 46);
+            this.Price.TabIndex = 4;
+            this.Price.TextChanged += new System.EventHandler(this.Price_TextChanged);
+            // 
             // AddOrder
             // 
-            this.AddOrder.Location = new System.Drawing.Point(43, 478);
+            this.AddOrder.Location = new System.Drawing.Point(214, 463);
             this.AddOrder.Name = "AddOrder";
             this.AddOrder.Size = new System.Drawing.Size(235, 39);
             this.AddOrder.TabIndex = 7;
@@ -57,51 +104,76 @@ namespace SuperMarket
             this.AddOrder.UseVisualStyleBackColor = true;
             this.AddOrder.Click += new System.EventHandler(this.AddOrder_Click);
             // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(43, 175);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(569, 282);
-            this.dataGridView1.TabIndex = 12;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
-            // 
             // Products
             // 
-            this.Products.Location = new System.Drawing.Point(250, 119);
+            this.Products.FormattingEnabled = true;
+            this.Products.Location = new System.Drawing.Point(235, 127);
             this.Products.Name = "Products";
-            this.Products.Size = new System.Drawing.Size(138, 37);
-            this.Products.TabIndex = 13;
-            this.Products.Text = "Products";
-            this.Products.UseVisualStyleBackColor = true;
-            this.Products.Click += new System.EventHandler(this.Products_Click);
+            this.Products.Size = new System.Drawing.Size(176, 28);
+            this.Products.TabIndex = 8;
+            this.Products.SelectedIndexChanged += new System.EventHandler(this.Products_SelectedIndexChanged);
             // 
-            // ViewOrder
+            // label4
             // 
-            this.ViewOrder.Location = new System.Drawing.Point(377, 478);
-            this.ViewOrder.Name = "ViewOrder";
-            this.ViewOrder.Size = new System.Drawing.Size(235, 39);
-            this.ViewOrder.TabIndex = 14;
-            this.ViewOrder.Text = "View Order";
-            this.ViewOrder.UseVisualStyleBackColor = true;
-            this.ViewOrder.Click += new System.EventHandler(this.ViewOrder_Click);
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Nova Cond", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(83, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 38);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Products";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Nova", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(78, 290);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(136, 38);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Quantity";
+            // 
+            // Quant
+            // 
+            this.Quant.BackColor = System.Drawing.Color.White;
+            this.Quant.Font = new System.Drawing.Font("Arial Nova Cond", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Quant.Location = new System.Drawing.Point(235, 288);
+            this.Quant.Name = "Quant";
+            this.Quant.Size = new System.Drawing.Size(176, 46);
+            this.Quant.TabIndex = 11;
+            this.Quant.TextChanged += new System.EventHandler(this.Qunat_TextChanged);
+            // 
+            // AddItem
+            // 
+            this.AddItem.FlatAppearance.BorderSize = 0;
+            this.AddItem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AddItem.Image = ((System.Drawing.Image)(resources.GetObject("AddItem.Image")));
+            this.AddItem.Location = new System.Drawing.Point(505, 239);
+            this.AddItem.Name = "AddItem";
+            this.AddItem.Size = new System.Drawing.Size(53, 43);
+            this.AddItem.TabIndex = 5;
+            this.AddItem.UseVisualStyleBackColor = true;
+            this.AddItem.Click += new System.EventHandler(this.AddItem_Click);
             // 
             // Order
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(670, 529);
-            this.Controls.Add(this.ViewOrder);
+            this.Controls.Add(this.Quant);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.Products);
-            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.AddOrder);
+            this.Controls.Add(this.AddItem);
+            this.Controls.Add(this.Price);
+            this.Controls.Add(this.Item);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Order";
             this.Text = "Order";
             this.Load += new System.EventHandler(this.Order_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -110,9 +182,15 @@ namespace SuperMarket
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Item;
+        private System.Windows.Forms.TextBox Price;
+        private CircleButton AddItem;
         private System.Windows.Forms.Button AddOrder;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button Products;
-        private System.Windows.Forms.Button ViewOrder;
+        private System.Windows.Forms.ComboBox Products;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Quant;
     }
 }
